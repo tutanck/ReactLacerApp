91638d605311cb6c93ba29cd47ae1f5dfb85d48f
Tutanck <tutanck@gmail.com>
2017-07-05 22:40:48 +0200

    first dirty test

--- App.js
+++ App.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+'use strict';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, Navigator, AsyncStorage } from 'react-native';
+
+import Signup from './src/pages/signup';
+import Account from './src/pages/account';
+
+import Header from './src/components/header';
+
+//import Firebase from 'firebase';
+
+
+//let app = new Firebase("YOUR-FIREBASE-APP-URL");
+
+import styles from './src/styles/common-styles.js';
+
+export default class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            component: null,
+            loaded: false
+        };
+    }
+
+    componentWillMount(){
+
+        AsyncStorage.getItem('user_data').then((user_data_json) => {
+
+            let user_data = JSON.parse(user_data_json);
+            let component = {component: Signup};
+            if(user_data != null){
+                app.authWithCustomToken(user_data.token, (error, authData) => {
+                    if(error){
+                        this.setState(component);
+                    }else{
+                        this.setState({component: Account});
+                    }
+                });
+            }else{
+                this.setState(component);
+            }
+        });
+
+    }
+
+
+
+    render(){
+
+        if(this.state.component){
+            return (
+                <Navigator
+                    initialRoute={{component: this.state.component}}
+                    configureScene={() => {
+                        return Navigator.SceneConfigs.FloatFromRight;
+                    }}
+                    renderScene={(route, navigator) => {
+                        if(route.component){
+                            return React.createElement(route.component, { navigator });
+                        }
+                    }}
+                />
+            );
+        }else{
+            return (
+                <View style={styles.container}>
+                    <Header text="React Native Firebase Auth" loaded={this.state.loaded} />
+                    <View style={styles.body}></View>
+                </View>
+            );
+        }
+
+    }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('App', () => App);
